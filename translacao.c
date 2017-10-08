4958ef929e8ab60f56c77e8071874a9c2f96db38
Ednux <edsonbrilhante@gmail.com>
2017-10-07 20:10:40 -0400

    Add function espelhamento

--- translacao.c
+++ translacao.c
@@ -10,6 +10,7 @@
 
 int translacao(SDL_Point *point, int tx, int ty);
 int escala(SDL_Point *point, int ref, int Sx, int Sy);
+int espelhamento(SDL_Point *point, int ref);
 
 int main(void) {
 	int i;
@@ -19,7 +20,7 @@ int main(void) {
 		{6,8}  //C
 	};
 	
-	escala(point, 0, 2, 2);
+	espelhamento(point, 2);
 	
 	for (i = 0; i < 3; i++) {
 		printf("[%d,%d]\n", point[i].x, point[i].y);
@@ -43,20 +44,12 @@ int translacao(SDL_Point *point, int tx, int ty)
 }
 int escala(SDL_Point *point, int ref, int Sx, int Sy)
 {
-	int tx, ty, i;
+	int tx, ty;
 	
 	tx = 0 - point[ref].x;
 	ty = 0 - point[ref].y;
 	
 	translacao(point, tx, ty);
-	
-	printf("tx %d, ty = %d\n", tx, ty);
-	
-	for (i = 0; i < 3; i++) {
-		printf("[%d,%d]\n", point[i].x, point[i].y);
-	}
-	
-	printf("\n");
 		
 	point[0].x = point[0].x * Sx;
 	point[0].y = point[0].y * Sy;
@@ -71,6 +64,39 @@ int escala(SDL_Point *point, int ref, int Sx, int Sy)
 	ty = ty * (-1);
 	
 	translacao(point, tx, ty);
+	
+	return 1;
+}
+int espelhamento(SDL_Point *point, int ref)
+{
+	switch (ref) {
+		//Somente em x
+		case 0:
+			point[0].x = point[0].x * (-1);
+			point[1].x = point[1].x * (-1);
+			point[2].x = point[2].x * (-1);
+			break;
+		//Somente em y
+		case 1:
+			point[0].y = point[0].y * (-1);
+			point[1].y = point[1].y * (-1);
+			point[2].y = point[2].y * (-1);
+			break;
+		//Em x e y
+		case 2:
+			point[0].x = point[0].x * (-1);
+			point[0].y = point[0].y * (-1);
+			
+			point[1].x = point[1].x * (-1);
+			point[1].y = point[1].y * (-1);
+			
+			point[2].x = point[2].x * (-1);
+			point[2].y = point[2].y * (-1);
+		
+			break;
+		default:
+			return 0;
+	}
 	
 	return 1;
 }
